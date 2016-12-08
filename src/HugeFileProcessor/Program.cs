bd6df2bf92d67b284453e46f79b85ab997866dda
mikhail-barg <mikhail.barg@gmail.com>
2016-12-08 14:43:07 +0300

    Added encoding option, added -chunk command

--- src/HugeFileProcessor/Program.cs
+++ src/HugeFileProcessor/Program.cs
@@ -12,8 +12,36 @@ namespace HugeFileProcessor
     {
         private const int VERBOSE_LINES_COUNT = 100000;
 
+        private static Encoding encoding = Encoding.Default;
+
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower() == "-enc" && args.Length > 1)
+                {
+                    string encodingName = args[1];
+                    
+                    int codePage;
+                    if (Int32.TryParse(encodingName, out codePage))
+                    {
+                        Console.WriteLine($"Setting encoding to CodePage {codePage}");
+                        encoding = Encoding.GetEncoding(codePage);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Setting encoding to {encodingName}");
+                        encoding = Encoding.GetEncoding(encodingName);
+                    }
+
+                    string[] argsCopy = new string[args.Length - 2];
+                    Array.Copy(args, 2, argsCopy, 0, argsCopy.Length);
+                    args = argsCopy;
+                }
+            }
+            Console.WriteLine($"Encoding is {encoding.WebName}");
+            Console.WriteLine();
+
             if (args.Length > 0)
             {
                 switch (args[0].ToLower())
@@ -47,12 +75,28 @@ namespace HugeFileProcessor
                         return;
                     }
                     break;
+                case "-chunk":
+                    if (args.Length == 3 || args.Length == 4)
+                    {
+                        int chunkSize = Int32.Parse(args[2]);
+                        int numberOfDigits = -1;
+                        if (args.Length == 4)
+                        {
+                            numberOfDigits = Int32.Parse(args[3]);
+                        }
+                        Chunkify(args[1], chunkSize, numberOfDigits);
+                    }
+                    break;
                 }
             }
             Console.WriteLine("Usage :");
-            Console.WriteLine("-split <sourceFile> <test>/<base> [<linesLimit>]\n\tsplits <sourceFile> to test and train, so that test file get (<test>/<base>) fraction of lines.\n\tSet 0 to <test> to skip test file creation.\n\t<linesLimit> - total number of lines to proces from <sourceFile>, set to -1 or skip to read all lines.\n\n");
-            Console.WriteLine("-shuffle <sourceFile> <batchSize> [<outFile>]\n\tshuffles lines from <sourceFile> to <outFile>.\n\t<batchSize> is in lines.\n\n");
-            Console.WriteLine("-count <sourceFile>\n\tjust count lines int <sourceFile>\n\n");
+            Console.WriteLine(" HugeFileProcessor.exe [-enc <encoding_name>] <command> <params>");
+            Console.WriteLine("");
+            Console.WriteLine("Commands :");
+            Console.WriteLine(" -split <sourceFile> <test>/<base> [<linesLimit>]\n\tsplits <sourceFile> to test and train, so that test file get (<test>/<base>) fraction of lines.\n\tSet 0 to <test> to skip test file creation.\n\t<linesLimit> - total number of lines to proces from <sourceFile>, set to -1 or skip to read all lines.\n\n");
+            Console.WriteLine(" -shuffle <sourceFile> <batchSize> [<outFile>]\n\tshuffles lines from <sourceFile> to <outFile>.\n\t<batchSize> is in lines.\n\n");
+            Console.WriteLine(" -count <sourceFile>\n\tjust count lines int <sourceFile>\n\n");
+            Console.WriteLine(" -chunk <sourceFile> <chunkSize> [<numberOfDigits>]\n\tsplit <sourceFile> into chunks, each having <chunkSize> lines.\n\tUse <numberOfDigits> to specify number fo digits in the resulting filename. If not specified, will count lines first\n\n");
             Environment.Exit(1);
         }
 
@@ -74,7 +118,7 @@ namespace HugeFileProcessor
 
             Console.WriteLine("Writing to file");
             DateTime start = DateTime.UtcNow;
-            using (StreamWriter writer = new StreamWriter(targetFileName))
+            using (StreamWriter writer = new StreamWriter(targetFileName, false, encoding))
             {
                 int batchIndex = 0;
                 for (int batchStart = 0; batchStart < linesCount; batchStart += batchSizeLines)
@@ -109,7 +153,8 @@ namespace HugeFileProcessor
             string[] writeLines = new string[batchSize];
 
 
-            using (StreamReader reader = File.OpenText(sourceFileName))
+            //using (StreamReader reader = File.OpenText(sourceFileName))
+            using (StreamReader reader = new StreamReader(sourceFileName, encoding))
             {
                 int lineIndex = -1;
                 foreach (KeyValuePair<int, int> pair in batchLines)
@@ -138,7 +183,7 @@ namespace HugeFileProcessor
             DateTime start = DateTime.UtcNow;
 
             int linesCount = 0;
-            foreach (string s in File.ReadLines(fileName))
+            foreach (string s in File.ReadLines(fileName, encoding))
             {
                 if (string.IsNullOrWhiteSpace(s))
                 {
@@ -203,12 +248,12 @@ namespace HugeFileProcessor
                 Console.WriteLine($"Limiting total lines number to {processLinesLimit}");
             }
 
-            using (StreamWriter trainWriter = new StreamWriter(sourceFileName + outFileInfix + ".train"))
+            using (StreamWriter trainWriter = new StreamWriter(sourceFileName + outFileInfix + ".train", false, encoding))
             {
-                using (StreamWriter testWriter = testFracUp <= 0 ? null : new StreamWriter(sourceFileName + outFileInfix + ".test"))
+                using (StreamWriter testWriter = testFracUp <= 0 ? null : new StreamWriter(sourceFileName + outFileInfix + ".test", false, encoding))
                 {
                     int lineIndex = 0;
-                    foreach (string sourceLine in File.ReadLines(sourceFileName))
+                    foreach (string sourceLine in File.ReadLines(sourceFileName, encoding))
                     {
                         ++lineIndex;
                         if ((lineIndex - 1) % testFracDown < testFracUp)
@@ -235,12 +280,84 @@ namespace HugeFileProcessor
         }
         #endregion
 
+        private static void Chunkify(string sourceFileName, int chunkSize, int numberOfDigits)
+        {
+            Console.WriteLine($"Splitting {sourceFileName} into chunks each, having {chunkSize} lines");
+            int chunksCount = -1;
+            if (numberOfDigits < 0)
+            {
+                Console.WriteLine($"numberOfDigits is not specified, counting lines");
+                TimeSpan timespan;
+                int linesCount = CountLines(sourceFileName, out timespan);
+                chunksCount = (linesCount / chunkSize) + (linesCount % chunkSize > 0 ? 1 : 0);
+                int tmp = chunksCount;
+                numberOfDigits = 0;
+                while (tmp > 0)
+                {
+                    ++numberOfDigits;
+                    tmp /= 10;
+                }
+                Console.WriteLine($"File contains {linesCount}, which gives {chunksCount} and {numberOfDigits} digits");
+            }
+
+            Console.WriteLine($"Starting");
+            int lineIndex = 0;
+            int chunkIndex = 1;
+            StreamWriter writer = null;
+            try
+            {
+                NewChunkWriter(ref writer, sourceFileName, chunkIndex, chunksCount, numberOfDigits);
+                foreach (string sourceLine in File.ReadLines(sourceFileName, encoding))
+                {
+                    ++lineIndex;
+                    if (lineIndex > chunkSize)
+                    {
+                        lineIndex = 0;
+                        ++chunkIndex;
+                        NewChunkWriter(ref writer, sourceFileName, chunkIndex, chunksCount, numberOfDigits);
+                    }
+                    writer.WriteLine(sourceLine);
+                }
+                writer.Close();
+                writer = null;
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer = null;
+                }
+            }
+            Console.WriteLine($"Done");
+        }
+
+        private static void NewChunkWriter(ref StreamWriter writer, string sourceFileName, int chunkIndex, int chunksCount, int numberOfDigits)
+        {
+            if (writer != null)
+            {
+                writer.Close();
+                writer = null;
+            }
+            string fileName = Path.ChangeExtension(sourceFileName, chunkIndex.ToString("D" + numberOfDigits) + Path.GetExtension(sourceFileName));
+            Console.WriteLine($"Writing to file {Path.GetFileName(fileName)}, chunk {chunkIndex} of " + (chunksCount >= 0 ? chunksCount.ToString() : "?"));
+            writer = new StreamWriter(fileName, false, encoding);
+        }
+
+        public static bool IsRunningOnMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+
         public static void RunLOHDefragmentation()
         {
             DateTime started = DateTime.UtcNow;
             //Console.WriteLine($"Collecting and defragging LOH... ");
 
-            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            if (!IsRunningOnMono())
+            {
+                GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            }
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
